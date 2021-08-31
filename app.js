0901cd7fca5fcec71338268990c62cfd4a098749
amreldib10 <72530029+amreldib10@users.noreply.github.com>
2021-08-31 23:07:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://Task-1:task123@cluster0.8zwuk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
+const AstrologyAPI = require('./astrology')
+
+const asyncApiCall = async () => {
+    const response = await AstrologyAPI.getCompatibility('Austin', '1987-05-21', 'Taylor', '1989-09-27')
+
+    console.log(response.data.data.Compatibility.heading)
+    console.log(response.data.data.Compatibility)
+}
+
+asyncApiCall()
